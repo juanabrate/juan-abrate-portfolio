bfef6ed9fdee5b09d7b5efb4ceb7f72b7da8967f
Juan Abrate <juan.abrate21@gmail.com>
2021-03-21 23:09:45 -0300

    particles 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,126 @@ import './Home.css';
 
 export default function Home() {
     return (
-        <div className='home'>
-            <Particles className='particles' width='100vw' height='100vh' 
+        <div className='home'>     
+
+                <Particles className='particles' width='95vw' height='30vh' 
                 params={{
                     particles: {
                         number: {
-                            value: 90
+                            value: 40
                         }},
                     interactivity: {
                         events: {
                             onHover: {
                                 enable: true,
-                                mode: 'attract'
+                                mode: 'slow'
+                            }                        
+                        },
+                        modes: {
+                            attract: {
+                                speed: 2,
+                                distance: 500,
+                                duration: 2
+                            },
+                            grab: {
+                                distance: 350,
+                                links: {
+                                    blink: false,
+                                    consent: true,
+                                    opacity: 2
+                                }
+                            },
+                            light: {
+                                area: {
+                                    radius: 400,
+                                    gradient: {
+                                        start: {
+                                            value: 'white'
+                                        },
+                                        stop: {
+                                            value: 'black'
+                                        }
+                                    }
+                                }
+                            },
+                            slow: {
+                                radius: 200,
+                                factor: 3
+                            }
+                        }
+                    }
+                }}/>
+
+            <div className='title-wrapper'>
+                <Particles className='particles8' width='27vw' height='35vh' 
+                    params={{
+                        particles: {
+                            number: {
+                                value: 20
+                            }},
+                        interactivity: {
+                            events: {
+                                onHover: {
+                                    enable: true,
+                                    mode: 'attract'
+                                }                        
+                            },
+                            modes: {
+                                attract: {
+                                    speed: 2,
+                                    distance: 500,
+                                    duration: 2
+                                }
+                            }
+                        }
+                    }}/>
+                    <div className='title'>
+                        <div className='text'>
+                            <span className="l">Hello, I'm <span className="juan">Juan Abrate</span></span>.
+                            <br/>
+                            I'm a full-stack web developer
+                        </div>
+                        <div className="button">
+                            <span className="button-text">
+                                My Skills
+                            </span>
+                        </div>                
+                    </div>
+                    <Particles className='particles8' width='27vw' height='35vh' 
+                    params={{
+                        particles: {
+                            number: {
+                                value: 20
+                            }},
+                        interactivity: {
+                            events: {
+                                onHover: {
+                                    enable: true,
+                                    mode: 'attract'
+                                }                        
+                            },
+                            modes: {
+                                attract: {
+                                    speed: 2,
+                                    distance: 500,
+                                    duration: 2
+                                }
+                            }
+                        }
+                    }}/>
+            </div>
+
+            <Particles className='particles4' width='95vw' height='30vh' 
+                params={{
+                    particles: {
+                        number: {
+                            value: 40
+                        }},
+                    interactivity: {
+                        events: {
+                            onHover: {
+                                enable: true,
+                                mode: 'slow'
                             }                        
                         },
                         modes: {
@@ -26,19 +134,9 @@ export default function Home() {
                             }
                         }
                     }
-                }}/>
-            <div className='text'>
-                <span className="l">H</span><span className="l">e</span><span className="l">l</span>
-                <span className="l">l</span><span className="l">o</span>, <span className="l">I'm </span>
-                <span className="juan">Juan Abrate</span>.
-                <br/>
-                I'm a full-stack web developer.
-            </div>
-            <div className="button">
-                <span className="button-text">
-                    My Skills
-                </span>
-            </div>
+                }}/> 
+
+
         </div>
     )
 }
